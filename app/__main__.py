789819b028b10002f17d9ffed0bd534271e3cb99
novoseltcev <novoseltcev.stanislav@gmail.com>
2023-01-17 15:11:54 +0300

    feat(setup): enable templates

--- app/__main__.py
+++ app/__main__.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.api import handlers
 from app.api import router as v1_router
 from app.core.settings import get_settings
-# from app.templates import router as templates_router
+from app.templates import router as templates_router
 
 app = FastAPI(
     title=get_settings().PROJECT_NAME,
@@ -13,5 +13,5 @@ app = FastAPI(
 )
 
 app.include_router(v1_router, prefix=get_settings().API_PREFIX)
-# app.include_router(templates_router)
+app.include_router(templates_router)
 handlers.register_api_error_handlers(app)
