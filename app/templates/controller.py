6b6ef6663be011359a790acbd3dd166f82f500e1
novoseltcev <novoseltcev.stanislav@gmail.com>
2023-01-17 16:33:23 +0300

    feat(templates): controller interact with api

--- app/templates/controller.py
+++ app/templates/controller.py
@@ -1,21 +1,18 @@
-from fastapi import APIRouter, Depends, Request
+import httpx
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-from app.api.base_schemas import Pagination
-from app.api.events.schemas import EventSchema
-from app.api.events.service import EventService
+from app.core.settings import get_settings
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/")
-async def index(
-    request: Request,
-    pagination: Pagination = Depends(),
-    service: EventService = Depends()
-):
-    raw_data, meta = await service.get_all(page=pagination.page, per_page=pagination.per_page)
-    data = [EventSchema.from_orm(event).dict() for event in raw_data]
-    print(f"Data: {data}")
-    return templates.TemplateResponse("index.html", {"request": request, "events": data})
+async def index(request: Request):
+    async with httpx.AsyncClient(http1=True) as client:
+        response = await client.get(f"http://localhost:5000{get_settings().API_PREFIX}/events/?page=1&perPage=100")
+        return templates.TemplateResponse("index.html", {
+            "request": request,
+            "events": response.json()["data"],
+        })
